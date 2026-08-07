d21de454e9c473879d35f62cb613219ca6f0be9b
Nguyễn Tấn Vĩnh <potter.harry0909@yahoo.com>
2026-08-07 23:16:37 +0700

    test

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/missingresources/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/missingresources/Fingerprints.kt
@@ -4,7 +4,7 @@ import app.revanced.util.fingerprint.legacyFingerprint
 import app.revanced.util.or
 import com.android.tools.smali.dexlib2.AccessFlags
 
-/*
+
 internal val navigationBarGetDrawableFingerprint = legacyFingerprint(
     name = "getDrawableFingerprint",
     accessFlags = AccessFlags.PUBLIC or AccessFlags.STATIC,
@@ -16,7 +16,7 @@ internal val navigationBarGetDrawableFingerprint = legacyFingerprint(
     }
 )
 
-*/
+
 
 internal val legacyIconEnumConverterFingerprint = legacyFingerprint(
     name = "legacyIconEnumConverterFingerprint",
